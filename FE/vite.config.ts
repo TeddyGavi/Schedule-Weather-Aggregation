84dfa124ae605ed1316dff05e540eea186207652
TeddyGavi <matc.davis@gmail.com>
2023-12-15 11:41:36 -0800

    testing shadcn

--- FE/vite.config.ts
+++ FE/vite.config.ts
@@ -6,8 +6,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src/'),
-    },
+    alias: [
+      { find: '@', replacement: path.resolve('./src/@/') },
+      {
+        find: '@/components',
+        replacement: path.resolve('./src/@/components/'),
+      },
+      { find: '@/lib', replacement: path.resolve('./src/@/lib/') },
+    ],
   },
 });
