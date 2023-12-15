35fe14de918bccd25b75b885d34310555d8e195d
TeddyGavi <matc.davis@gmail.com>
2023-12-14 16:09:51 -0800

    init db setup

---
+++ BE/db/db.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        name: 'default',
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        autoLoadEntities: true,
+        synchronize: true,
+        retryDelay: 10000,
+        retryAttempts: 10,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }),
+    }),
+  ],
+  providers: [],
+  exports: [],
+})
+export class DBModule {}
